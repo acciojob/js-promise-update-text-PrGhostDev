232a0e8fec7da9e0ab077153e6190a7df6048ed5
PrGhostDev <139947007+PrGhostDev@users.noreply.github.com>
2023-11-12 20:05:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateOutputText() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const newText = 'Hello, world!';
+      resolve(newText);
+    }, 1000); 
+  });
+}
+
+updateOutputText().then((text) => {
+  const outputElement = document.getElementById('output');
+  outputElement.innerText = text;
+});
